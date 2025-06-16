e62f1a7f783a072944b758fbec0ee10a615df049
Rich Plastow <rich@richplastow.com>
2025-06-16 11:34:55 +0100

    adds tests

--- test.mjs
+++ test.mjs
@@ -4,20 +4,24 @@
  * npm test
  */
 
-import { deepEqual as eq, throws } from "node:assert";
-import { testParseMarkdownContent } from "./lib/parse-markdown-content.mjs";
-import { testParseShellCodeBlock } from "./lib/parse-shell-code-block.mjs";
+import util from 'util';
+import { deepEqual as eq, throws } from 'node:assert';
+import { testParseRawMarkdown } from './lib/parse-raw-markdown.mjs';
+import { testParseShellCodeBlock } from './lib/parse-shell-code-block.mjs';
+
+// Set the default depth to Infinity to remove the nested object depth limit.
+util.inspect.defaultOptions.depth = Infinity;
 
 // Check that eq() and throws() work as expected.
-eq({a:1}, {a:1}, "eq() should duck-type check objects");
+eq({a:1}, {a:1}, 'eq() should duck-type check objects');
 throws(
-  () => { throw RangeError("Some message") },
-  { message: /Some message/, name: "RangeError" },
+  () => { throw RangeError('Some message') },
+  { message: /Some message/, name: 'RangeError' },
   "throws() should check the error message and type",
 );
 
 // Test library functions.
-testParseMarkdownContent(eq, throws);
+testParseRawMarkdown(eq, throws);
 testParseShellCodeBlock(eq, throws);
 
 // Test the main entry point.
